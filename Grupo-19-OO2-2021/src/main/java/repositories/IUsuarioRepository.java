7319cbfb2187891209c4e5f62081ef6f052c3168
franmsn2011 <51340801+franmsn2011@users.noreply.github.com>
2021-05-26 18:07:02 -0300

    repositorios

---
+++ Grupo-19-OO2-2021/src/main/java/repositories/IUsuarioRepository.java
@@ -0,0 +1,23 @@
+package repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import entities.Usuario;
+@Repository("usuarioRepository")
+public interface IUsuarioRepository extends JpaRepository<Usuario, Serializable>{
+
+	public abstract Usuario findByIdUsuario(int id);
+	
+	public abstract Usuario findByNombreUsuario(String name);
+	
+	@Query("SELECT u FROM Usuario u WHERE u.nombreUsuario= (:nombreUsuario)")
+	public abstract Usuario findByNombreUsuarioAndFetchPerfilEagerly(@Param("nombreUsuario") String nombreUsuario);
+	/*@Query("SELECT u FROM Usuario u JOIN fetch Perfil p on u.idPerfil =  p.idPerfil WHERE u.nombreUsuario= (:nombreUsuario)")
+	public abstract Usuario findByNombreUsuarioAndFetchPerfilEagerly(@Param("nombreUsuario") String nombreUsuario);
+*/
+}
